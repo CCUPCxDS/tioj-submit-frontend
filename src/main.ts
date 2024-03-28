e80693059ff73a9cbf285038d3e2ed5e7ed301aa
as6325400 <105158172+as6325400@users.noreply.github.com>
2024-03-28 21:24:56 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './css/index.css'
+import VueUploadComponent from 'vue-upload-component'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.component('file-upload', VueUploadComponent)
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
-
+import upload from "../components/upload.vue";
 </script>
 
 <template>
-  <div></div>
+  <form method="post" action="">
+    <upload />
+    <button type="submit" class="">Submit</button>
+  </form>
 </template>
