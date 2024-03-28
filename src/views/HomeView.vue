476cf20bc055ad44a974c7dc1a028065b3602c4c
syzygy608 <hankwu99@gmail.com>
2024-03-28 22:22:45 +0800

    add css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,15 @@ import upload from "../components/upload.vue";
 </script>
 
 <template>
-  <form method="post" action="">
-    <upload />
-    <button type="submit" class="">Submit</button>
-  </form>
+  <div class = "flex justify-center items-center" id = "content">
+    <form method="post" action="" class = "flex justify-center flex-col">
+      <upload class = "w-[30rem]"/>
+      <input type = "text" name="problem" placeholder="Problem ID" class="data-input">
+      <input type = "text" name="user" placeholder="Judge 帳號" class="data-input">
+      <input type = "password" name="password" placeholder="Judge 密碼" class="data-input">
+      <button type="submit" class="text-xl bg-purple-400 p-3 rounded-lg m-2 hover:bg-purple-500">
+        Submit
+      </button>
+    </form>
+  </div>
 </template>
